59a4692650420630e0909a9fb414967c2a279905
Michael Langford <michael.langford@sap.com>
2022-10-25 12:46:18 -0400

    Added sadface and munch fade in

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 window.addEventListener('load', event => {
 
-	const consentCheck = () => {
+	if(localStorage.getItem('optout') !== 'true'){
+
+		const gaScript = document.createElement('script');
+
+		gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-C6WJMPH8E1';
+		document.body.append(gaScript);
+
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-C6WJMPH8E1');
+
+	}
+
+
+	const consentCheck = ( _ => {
 
 		const optOutLink = document.getElementById('optout')
 		const optInLink = document.getElementById('optin')
 
 		if(localStorage.getItem('optout') === 'true'){
-			document.getElementById('optinstring').style.display = 'block';
+			document.getElementById('optinstring').classList.remove('hide')
+			document.getElementById('optinstring').classList.add('show');
 		}
 		else{
-			document.getElementById('optoutstring').style.display = 'block';
+			document.getElementById('optoutstring').classList.remove('hide')
+			document.getElementById('optoutstring').classList.add('show');
 		}
 
 		optOutLink.addEventListener('click', event => {
@@ -20,27 +37,27 @@ window.addEventListener('load', event => {
 			localStorage.removeItem('optout');
 		});
 
+	})();
+
+
+	const showMunch = (event) => {
+
 	}
 
 
-
-    if(localStorage.getItem('optout') !== 'true'){
-
-		const gaScript = document.createElement('script');
-
-		gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-C6WJMPH8E1';
-        document.body.append(gaScript);
-    
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		gtag('config', 'G-C6WJMPH8E1');
-
-    }
+});
 
 
+document.getElementById('sadface-svg').addEventListener('click', event => {
+	const target = event.target;
+	const showElement = document.getElementById('munch-container');
+	const hideElement = document.getElementById('sadface-container');
 
-    consentCheck();
+	showElement.classList.remove('hide');
+	showElement.classList.add('show');
 
-})
+	hideElement.classList.remove('show');
+	hideElement.classList.add('hide');
 
+
+});
